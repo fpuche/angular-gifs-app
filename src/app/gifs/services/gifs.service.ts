e83e1db4e7fc5a7ce66eb605b710d377ba1fa1d9
Fernando Puche <fdopuche@gmail.com>
2024-02-14 21:59:42 +0100

    Gif images as search result

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
+
+@Injectable({providedIn: 'root'})
+export class GifsService {
+
+  public gifList:Gif[] = [];
+  private _tagsHistory  :string[] = [];
+  private apiKey        :string = 'HR4MwtSIITxQGGNBHEkCKpHvkyVxoZiY';
+  private serviceUrl    :string = 'https://api.giphy.com/v1/gifs';
+
+  constructor( private http: HttpClient ) { }
+
+  get TagHistory(){
+    return [...this._tagsHistory];
+  }
+
+  private organizeHistory( tag:string ) {
+    tag = tag.toLowerCase();
+
+    if( this._tagsHistory.includes(tag) ){
+      this._tagsHistory = this._tagsHistory.filter( (oldTag) => oldTag !== tag );
+    }
+
+    this._tagsHistory.unshift( tag );
+    this._tagsHistory = this._tagsHistory.splice( 0,10 );
+
+  }
+
+  searchTag( tag:string ):void {
+
+    if( tag.length === 0 ) return;
+    this.organizeHistory(tag);
+
+    const params = new HttpParams()
+    .set('api_key', this.apiKey)
+    .set('limit', '10')
+    .set('q', tag);
+
+    this.http.get<SearchResponse>(`${ this.serviceUrl }/search`, { params:params })
+    .subscribe( resp => {
+      this.gifList = resp.data;
+      //console.log( {gifs: this.gifList} );
+    });
+
+  }
+
+}
