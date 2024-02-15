2c9dfddef6a95b96d2f77a0ba676a2bcfb151810
Fernando Puche <fdopuche@gmail.com>
2024-02-15 21:53:03 +0100

    Search saved in Local Storage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -10,7 +10,10 @@ export class GifsService {
   private apiKey        :string = 'HR4MwtSIITxQGGNBHEkCKpHvkyVxoZiY';
   private serviceUrl    :string = 'https://api.giphy.com/v1/gifs';
 
-  constructor( private http: HttpClient ) { }
+  constructor( private http: HttpClient ) {
+    this.loadLocalStorage();
+    console.log('Gifs Service Ready');
+  }
 
   get TagHistory(){
     return [...this._tagsHistory];
@@ -25,6 +28,20 @@ export class GifsService {
 
     this._tagsHistory.unshift( tag );
     this._tagsHistory = this._tagsHistory.splice( 0,10 );
+    this.saveLocalStorage();
+
+  }
+
+  private saveLocalStorage():void{
+    localStorage.setItem( 'history', JSON.stringify( this._tagsHistory ) );
+  }
+
+  private loadLocalStorage():void {
+    if( !localStorage.getItem( 'history' ) ) return;
+    this._tagsHistory = JSON.parse( localStorage.getItem( 'history' )! );
+
+    if(this._tagsHistory.length === 0) return;
+    this.searchTag(this._tagsHistory[0]);
 
   }
 
